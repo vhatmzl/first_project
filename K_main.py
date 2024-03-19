451504cdf59e01cfd46ef386201f91c90d5fbe18
Your Name <Your Email>
2024-03-19 10:43:04 +0900

    all finish

--- K_main.py
+++ K_main.py
@@ -4,13 +4,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from K_sub_main import *
 
-# 상태 저장
+
 if 'page' not in st.session_state:
     st.session_state['page'] = 'HOME'
 
 menus = {"HOME": home, "지역별 인구 변화": kchange, "지역별 인구 비교": kbigyo, "연도별 변화": kyear, "지도": ksido}
 
+
 with st.sidebar:
+    sidebar_style = """
+        <style>
+            .sidebar .sidebar-content {
+                background-color: #3498db;
+                color: white;
+            }
+        </style>
+    """
+    st.markdown(sidebar_style, unsafe_allow_html=True)
     for menu in menus.keys():
         if st.button(menu, use_container_width=True, type='primary' if st.session_state['page']==menu else 'secondary'):
             st.session_state['page']=menu
